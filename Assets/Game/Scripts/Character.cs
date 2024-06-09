edb904baf4c7fdbefd37aaf59e3722ab9d19dbf3
Barış Öktem <oktembaris77@gmail.com>
2024-06-10 00:12:59 +0300

    Character modeled. Rig added. Walking and idle animations added. Lighting done.

--- Assets/Game/Scripts/Character.cs
+++ Assets/Game/Scripts/Character.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
 	// Hýz ve zýplama gücü parametreleri
+	[SerializeField] Animator anim;
 	public float moveSpeed = 5f;
 	public float jumpForce = 5f;
-	public float mouseSpeed = 5f;
-	
+	public float mouseXSpeed = 5f;
+	public float mouseYSpeed = 5f;
+
 
 	// Yer çekimi ve karakter kontrolcü referanslarý
 	[SerializeField] bool isGrounded;
 	private float gravity = -9.81f;
 	[SerializeField] private Vector3 velocity;
 	private CharacterController controller;
+
+	[SerializeField] GameObject cam;
+
+	public float maxLookAngle = 90f; // Maksimum bakýþ açýsý
 
 	private void Start()
 	{
@@ -26,13 +32,22 @@ public class Character : MonoBehaviour
 	{
 		MoveUpdate();
 
-
+		Debug.Log(cam.transform.rotation.x);
 	}
 	private void MoveUpdate()
 	{
 		// Yatay ve dikey hareket vektörleri
 		float horizontalMove = Input.GetAxis("Horizontal");
 		float verticalMove = Input.GetAxis("Vertical");
+
+		if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+		{
+			Walk();
+		}
+		else
+		{
+			AllFalse();
+		}
 
 		// Check if the character is grounded
 		isGrounded = controller.isGrounded;
@@ -60,7 +75,36 @@ public class Character : MonoBehaviour
 		velocity.y += gravity * Time.deltaTime;
 		controller.Move(velocity * Time.deltaTime);
 
+		float rotX = Mathf.Clamp(cam.transform.rotation.x, -0.2f, 0.2f);
+
 		//Mouse rotate
-		transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * mouseSpeed, 0);
+		transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * mouseXSpeed, 0);
+		//cam.transform.Rotate(-Input.GetAxis("Mouse Y") * Time.deltaTime * mouseYSpeed, 0, 0);
+
+
+
+		// Fare eksenlerini al
+		float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseYSpeed;
+
+		// Nesnenin dönme açýsýný hesapla
+		float newRotationX = cam.transform.localEulerAngles.x - mouseY;
+
+		// Aþýrý dönme durumunu düzelt
+		if (newRotationX > 180)
+			newRotationX -= 360;
+
+		// Maksimum ve minimum bakýþ açýsýný sýnýrla
+		newRotationX = Mathf.Clamp(newRotationX, -maxLookAngle, maxLookAngle);
+
+		// Yeni dönme açýsýný uygula
+		cam.transform.localEulerAngles = new Vector3(newRotationX, cam.transform.localEulerAngles.y, cam.transform.localEulerAngles.z);
+	}
+	public void AllFalse()
+	{
+		anim.SetBool("walk", false);
+	}
+	public void Walk()
+	{
+		anim.SetBool("walk", true);
 	}
 }
